9f100e9cd460d9c1e34aed6aa06d8b9a867c83a1
Matt1899 <danny20001102@gmail.com>
2022-07-16 21:32:40 +0800

    set up environment and event listeners

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
 import dotenv from 'dotenv'
 import discord from 'discord.js'
-import { Client, Intents } from "discord.js"
-
 import utility from './utility'
+
+//configure environment
 dotenv.config()
-const client = new discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+//set bot token from .env file
 const token = process.env.TOKEN
+//set bot intents
+const client = new discord.Client({ intents: [discord.Intents.FLAGS.GUILDS, discord.Intents.FLAGS.GUILD_MESSAGES] });
 
-client.login(token)
-
+//an event listener that prints the logging message when the bot is activated
 client.on('ready', ()=>{
     console.info(`Logged in as ${client.user!.tag}!`);
 })
 
+//an event listener that is called whenever users enter message in the Discord server
 client.on("messageCreate", (message)=>{
     utility.setUpCommandReact(message)
 })
